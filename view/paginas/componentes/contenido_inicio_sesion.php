e9e50a0274f034aaf78c8e575ed39188ad70bafe
David Israel <israeldavi0904@gmail.com>
2022-05-22 17:10:07 -0500

    validacion

--- view/paginas/componentes/contenido_inicio_sesion.php
+++ view/paginas/componentes/contenido_inicio_sesion.php
@@ -1,21 +1,21 @@
 <main class="Main--register">
-        <div class="Imagen"></div>
-        <div class="form__container">
-            <h2 class="form__container__title">Inicio de Sesion</h2>
-            <form action="controller/acceso/login.php" method="post" class="form__container">
+    <div class="Imagen"></div>
+    <div class="form__container">
+        <h2 class="form__container__title">Inicio de Sesion</h2>
+        <form action="controller/acceso/login.php" method="post" class="form__container">
 
-                <label for="dni">
-                    <span id="subir">Ingresa tu DNI</span>
-                    <input class="form__container__input" type="text" name="dni" id="dni" required>
-                </label>
+            <label for="dni">
+                <span id="subir">Ingresa tu DNI</span>
+                <input class="form__container__input" type="text" name="dni" id="dni" required maxlength="8" oninput="this.value = this.value.replace(/[^0-9]/g, '').replace(/(\..*)\./g, '$1');">
+            </label>
 
-                <label for="clave">
-                    <span id="subir">Ingresa tu Clave</span>
-                    <input class="form__container__input" type="password" name="clave" id="clave" maxlength="6" required>
-                </label>
+            <label for="clave">
+                <span id="subir">Ingresa tu Clave</span>
+                <input class="form__container__input" type="password" name="clave" id="clave" maxlength="6" required>
+            </label>
 
-                <input class="form__container__button" type="submit" value="Entrar">
-                <small>¿No tienes una cuenta? <a href="index.php?pagina=registro" class="form__container__small">Registrate</a></small>
-            </form>
-        </div>
+            <input class="form__container__button" type="submit" value="Entrar">
+            <small>¿No tienes una cuenta? <a href="index.php?pagina=registro" class="form__container__small">Registrate</a></small>
+        </form>
+    </div>
 </main>
